19957a2dbbf8dbee6f95c950c4c057452d8c4232
João Miguel Moscardini <joaomiguelmv_04@hotmail.com>
2024-04-12 16:10:10 -0300

    feat: script insert values

--- scripts/20240412000001_create_initial_tables.sql
+++ scripts/20240412000001_create_initial_tables.sql
@@ -13,55 +13,57 @@ CREATE TABLE users (
 
 CREATE TYPE client_type AS ENUM ('F', 'J');
 CREATE TABLE public.clients (
-    id SERIAL PRIMARY KEY,
-    "type" public."client_type" NOT NULL,
-    "name" varchar(200) NOT NULL,
-    "document" varchar(200) NOT NULL,
-    observation varchar(255),
-    active bool DEFAULT true,
-    created_at timestamp DEFAULT current_timestamp,
-    updated_at timestamp,
-    deleted_at timestamp
+  id SERIAL PRIMARY KEY,
+  "type" public."client_type" NOT NULL,
+  "name" varchar(200) NOT NULL,
+  "document" varchar(200) NOT NULL,
+  observation varchar(255),
+  active bool DEFAULT true,
+  created_at timestamp DEFAULT current_timestamp,
+  updated_at timestamp,
+  deleted_at timestamp
 );
 
 CREATE TYPE profile_type AS ENUM ('conservative', 'moderate', 'aggressive');
 CREATE TABLE public.profile (
-    id SERIAL PRIMARY KEY,
-    "type" public."profile_type" NOT null,
-    description VARCHAR (255) NOT null,
-    created_at timestamp DEFAULT current_timestamp,
-    updated_at timestamp,
-    deleted_at timestamp
+  id SERIAL PRIMARY KEY,
+  "type" public."profile_type" NOT null,
+  description VARCHAR (255) NOT null,
+  created_at timestamp DEFAULT current_timestamp,
+  updated_at timestamp,
+  deleted_at timestamp
 );
 
 
 CREATE TABLE public.listed_shares (
-    id SERIAL PRIMARY KEY,
-    "ticker" varchar(200) NOT NULL,
-    "name" varchar(200) NOT NULL,
-    b3_sector_classification varchar(200) DEFAULT NULL,
-    id_profile int not null,
-    created_at timestamp DEFAULT current_timestamp,
-    updated_at timestamp,
-    deleted_at timestamp,
-    FOREIGN KEY (id_profile) REFERENCES profile(id)
+  id SERIAL PRIMARY KEY,
+  "ticker" varchar(200) NOT NULL,
+  "name" varchar(200) NOT NULL,
+  b3_sector_classification varchar(200) DEFAULT NULL,
+  id_profile int not null,
+  created_at timestamp DEFAULT current_timestamp,
+  updated_at timestamp,
+  deleted_at timestamp,
+  FOREIGN KEY (id_profile) REFERENCES profile(id)
 );
 
 
 CREATE TABLE public.listed_share_history (
 	id SERIAL PRIMARY KEY,
 	id_listed_shares int not null,
-	date timestamp,
-    last_value numeric(15, 4) NOT NULL,
-    opening numeric(15, 4) NOT NULL,
-    high numeric(15, 4) NOT NULL,
-    low numeric(15, 4) NOT NULL,
-    trading_volume numeric(15, 4) NOT NULL,
-    percentage_change numeric(15, 4) NOT NULL,
-    created_at timestamp DEFAULT current_timestamp,
-    updated_at timestamp,
-    deleted_at timestamp,
-    FOREIGN KEY (id_listed_shares) REFERENCES listed_shares(id)
+	"date" date default null,
+  last_value numeric(15, 4) NOT NULL,
+  opening numeric(15, 4) NOT NULL,
+  high numeric(15, 4) NOT NULL,
+  low numeric(15, 4) NOT NULL,
+  trading_volume varchar(100) default null,
+  percentage_change numeric(15, 4) NOT NULL,
+  id_profile int not null,
+  created_at timestamp DEFAULT current_timestamp,
+  updated_at timestamp,
+  deleted_at timestamp,
+  FOREIGN KEY (id_listed_shares) REFERENCES listed_shares(id),
+  FOREIGN KEY (id_profile) REFERENCES profile(id)
 );
 
 
@@ -69,12 +71,12 @@ CREATE TABLE public.investment_portfolio (
 	id SERIAL PRIMARY KEY,
 	id_client int not null,
 	id_listed_shares int not null,
-    share_price numeric(15, 4) NOT NULL,
-    quantity_purchased int not null,
-    invested_amount numeric(15, 4) NOT NULL,
-    created_at timestamp DEFAULT current_timestamp,
-    updated_at timestamp,
-    deleted_at timestamp,
-    FOREIGN KEY (id_client) REFERENCES clients(id),
-    FOREIGN KEY (id_listed_shares) REFERENCES listed_shares(id)
+  share_price numeric(15, 4) NOT NULL,
+  quantity_purchased int not null,
+  invested_amount numeric(15, 4) NOT NULL,
+  created_at timestamp DEFAULT current_timestamp,
+  updated_at timestamp,
+  deleted_at timestamp,
+  FOREIGN KEY (id_client) REFERENCES clients(id),
+  FOREIGN KEY (id_listed_shares) REFERENCES listed_shares(id)
 );

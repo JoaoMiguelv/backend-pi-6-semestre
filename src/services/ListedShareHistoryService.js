31ad945f557ed2698e7d28f5d7071e33f35922b2
João Miguel Moscardini <joaomiguelmv_04@hotmail.com>
2024-05-11 11:24:27 -0300

    feat: definição do id_profile pela IA

--- src/services/ListedShareHistoryService.js
+++ src/services/ListedShareHistoryService.js
@@ -1,10 +1,48 @@
 
 import CommonService from '../commons/CommonService.js';
 import { ListedShareHistory } from '../models/ListedShareHistoryModel.js';
+import { BadRequestError } from '../errors/index.js';
+import axios from "axios";
 
 export default class ListedShareHistoryService extends CommonService {
   constructor() {
     super(ListedShareHistory);
     this.listedShareHistoryModel = ListedShareHistory;
+  }
+
+  async create(req) {
+    delete req.body.id_profile; // id_profile será definido pela IA
+
+    const body = req.body;
+    const orderedArray = [
+      body.last_value,
+      body.opening,
+      body.high,
+      body.low,
+      body.trading_volume,
+      body.percentage_change,
+      body.id_listed_shares
+    ];
+
+    console.log(orderedArray);
+
+    const reqOptions = {
+      url: "https://training-pi-6.onrender.com/training",
+      method: "POST",
+      headers: { "Accept": "*/*", "Content-Type": "application/json" },
+      data: JSON.stringify({ "data": orderedArray }),
+    };
+
+    try {
+      const response = await axios.request(reqOptions);
+      console.log(response.data);
+      req.body.id_profile = response.data.id_profile;
+    } catch (error) {
+      throw new BadRequestError(error);
+    }
+
+    console.log(req.body);
+
+    return await super.create(req);
   }
 }
